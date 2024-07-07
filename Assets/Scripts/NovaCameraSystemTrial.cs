cef3c79180c3c7615a0df1c2c1f8638a65b5756a
A.M <82979811+MournTHEMorning@users.noreply.github.com>
2024-07-07 15:20:35 -0400

    Initial Commit (Only scrolling/basic img)
    All progress up until July 7th, 2024. Includes:
    -Circumpolar (first 5 imgs, OLD, and basic scrollview from tutorial)
    -No sound/SFX -- literally only scrolling
    -This is just the Assets folder with Sprites, Scripts, Scenes, Backgrounds_CGs folders
    -Has older assets as well (ex: unselected/selected star, eyes opening etc.)
    
    **All images are hand drawn by me and should not be used for anything outside this project

---
+++ Assets/Scripts/NovaCameraSystemTrial.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NovaCameraSystemTrial : MonoBehaviour
+{
+    //Based on this: https://youtu.be/pJQndtJ2rk0?t=252 
+        //not done yet obvs lol
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
